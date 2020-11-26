2102568c4159d2e669baf130c2bb7009e123c296
Salih Candir <salih.candir@styleflasher.at>
2020-11-26 09:42:28 +0100

    add suggestedAnswer and answerCount

--- src/ContextTypes/Question.php
+++ src/ContextTypes/Question.php
@@ -11,7 +11,9 @@ class Question extends CreativeWork
      * @var array
      */
     protected $extendedStructure = [
+        'answerCount' => null,
         'acceptedAnswer' => Answer::class,
+        'suggestedAnswer' => Answer::class,
     ];
 
     /**
@@ -23,5 +25,21 @@ class Question extends CreativeWork
     public function __construct(array $attributes, array $extendedStructure = [])
     {
         parent::__construct($attributes, array_merge($this->structure, $this->extendedStructure, $extendedStructure));
+    }
+
+
+    protected function setSuggestedAnswerAttribute($items)
+    {
+        if (is_array($items) === false) {
+            return $items;
+        }
+
+        if (is_array(reset($items)) === false) {
+            return $this->getNestedContext(Answer::class, $items);
+        }
+
+        return array_map(function ($item) {
+            return $this->getNestedContext(Answer::class, $item);
+        }, $items);
     }
 }
